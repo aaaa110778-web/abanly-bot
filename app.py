7371fb756f06942f5ab6b47fe9573404a10f84e6
aaaa110778-web <aaaa110778@gmail.com>
2025-08-07 03:17:42 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-from datetime import datetime, timedelta
-import time, requests, pandas, lxml
-from lxml import html
-import telegram
-from flask import Flask, request
 import re
+from flask import Flask, request
+import telegram
+import asyncio
+import yahoo_fin.stock_info as si
+import pandas
 
+
+TOKEN = '<YOUR BOT API'
+bot = telegram.Bot(token=TOKEN)
 
 app = Flask(__name__)
-
-TOKEN = 'YOUR BOT API TOKEN'
-bot = telegram.Bot(token=TOKEN)
 
 
 @app.route('/')
@@ -19,7 +19,7 @@ def index():
 
 @app.route('/setwebhook', methods=['GET', 'POST'])
 def set_webhook():
-    s = bot.setWebhook('https://YOUR BOT NAME.herokuapp.com/{HOOK}'.format(HOOK=TOKEN))
+    s = bot.setWebhook('https://<YOUR APP NAME>.herokuapp.com/{HOOK}'.format(HOOK=TOKEN))
     if s:
         return "webhook setup ok"
     else:
@@ -33,27 +33,34 @@ def respond():
 
     chat_id = update.message.chat.id
     msg_id = update.message.message_id
+
     # Telegram understands UTF-8, so encode text for unicode compatibility
     text = update.message.text.encode('utf-8').decode()
     # for debugging purposes only
+    print("got text message :", text)
     # the first time you chat with the bot AKA the welcoming message
     if text == "/start":
         # print the welcoming message
         bot_welcome = """
-        Welcome to Dividends History bot.
-        The bot sends the dividend history for the requested stock symbol.
-        Please enter only the ticket symbol and not the company name
+        Welcome to Stocks Fundamental analysis bot.
+        The bot sends the fundamental analysis data of the requested stock symbol.
         """
-        print('Received a start request from chat id ' + str(chat_id) + ' and msg id ' + str(msg_id))
         # send the welcoming message
         bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
+
     else:
         try:
             # clear the message we got from any non alphabets
+            config = {}
             text = re.sub(r"\W", "_", text)
-            print('Getting data for company ' + text.upper())
-            data = process_data(text.upper())
-            bot.sendMessage(chat_id=chat_id, text=data)
+            company = Company(text.upper())
+            print('getting data for company ' + text.upper())
+
+            get_fundamental_indicators_for_company(config, company)
+            result = ""
+            for key in company.fundamental_indicators:
+                result += '{:<30}{:40}\n'.format(str(key), str(company.fundamental_indicators[key]))
+            bot.sendMessage(chat_id=chat_id, text=result)
         except Exception as e:
             print('Exception is ' + str(e))
             # if things went wrong
@@ -62,80 +69,95 @@ def respond():
     return 'ok'
 
 
-def format_date(date_datetime):
-    date_timetuple = date_datetime.timetuple()
-    date_mktime = time.mktime(date_timetuple)
-    date_int = int(date_mktime)
-    date_str = str(date_int)
-    return date_str
+def get_fundamental_indicators_for_company(config, company):
+    print('getting the fundamental indicators')
+    company.fundamental_indicators = {}
+
+    data = si.get_quote_table(company.symbol.upper())
+    company.fundamental_indicators = data
+
+    keys = {
+        'Market Cap (intraday) 5': 'MarketCap',
+        'Price/Sales (ttm)': 'PS',
+        'Trailing P/E': 'PE',
+        'PEG Ratio (5 yr expected) 1': 'PEG',
+        'Price/Book (mrq)': 'PB'
+    }
+
+    data = si.get_stats_valuation(company.symbol)
+    get_data_item(company.fundamental_indicators, data, keys)
+
+    # Income statement and Balance sheet
+    data = get_statatistics(company.symbol)
+    get_data_item(company.fundamental_indicators, data,
+                {
+                    'Profit Margin': 'ProfitMargin',
+                    'Operating Margin (ttm)': 'OperMargin',
+                    'Current Ratio (mrq)': 'CurrentRatio',
+                    'Payout Ratio 4': 'DivPayoutRatio'
+                })
+
+    get_last_data_item(company.fundamental_indicators, data,
+            {
+                'Return on assets': 'ROA',
+                'Return on equity': 'ROE',
+                'Total cash per share': 'Cash/Share',
+                'Book value per share': 'Book/Share',
+                'Total debt/equity': 'Debt/Equity'
+            })
 
 
-def subdomain(symbol, start, end):
-    format_url = "{0}/history?period1={1}&period2={2}"
-    tail_url = "&interval=div%7Csplit&filter=div&frequency=1d"
-    subdomain = format_url.format(symbol, start, end) + tail_url
-    return subdomain
+class Company:
+    def __init__(self, symbol):
+        self.symbol = symbol
+        self.fundamental_indicators = {}
 
 
-def header(subdomain):
-    hdrs = {"authority": "finance.yahoo.com",
-            "method": "GET",
-            "path": subdomain,
-            "scheme": "https",
-            "accept": "text/html,application/xhtml+xml",
-            "accept-encoding": "gzip, deflate, br",
-            "accept-language": "en-US,en;q=0.9",
-            "cache-control": "no-cache",
-            "dnt": "1",
-            "pragma": "no-cache",
-            "sec-fetch-mode": "navigate",
-            "sec-fetch-site": "same-origin",
-            "sec-fetch-user": "?1",
-            "upgrade-insecure-requests": "1",
-            "user-agent": "Mozilla/5.0"}
-    return hdrs
+def to_float(val):
+    if val == 0:
+        return float(0)
+
+    val = str(val).upper()
+
+    if '%' in val:
+        return round(float(val[:-1]), 4)
+
+    m = {'K': 3, 'M': 6, 'B': 9, 'T': 12}
+
+    for key in m.keys():
+        if key in val:
+            multiplier = m.get(val[-1])
+            return round(float(val[:-1]) * (10 ** multiplier), 4)
+    return round(float(val), 4)
 
 
-def scrape_page(url, header):
-    page = requests.get(url)
-    element_html = html.fromstring(page.content)
-    table = element_html.xpath('//table')
-    table_tree = lxml.etree.tostring(table[0], method='xml')
-    pandas.set_option("display.max_rows", None, "display.max_columns", None)
-    panda = pandas.read_html(table_tree)
-    return panda
+def get_statatistics(symbol):
+    url = f"https://finance.yahoo.com/quote/{symbol}/key-statistics?p={symbol}"
+    dataframes = pandas.read_html(url)
+    return pandas.concat(dataframes[1:])
 
 
-def clean_dividends(symbol, dividends):
-    index = len(dividends)
-    dividends = dividends.drop(index-1)
-    dividends = dividends.set_index('Date')
-    dividends = dividends['Dividends']
-    dividends = dividends.str.replace(r'\Dividend', '', regex=True)
-    dividends = dividends.astype(float)
-    dividends.name = symbol
-    return dividends
+def get_data_item(result, dataframe, columns):
+    for column_to_find, column_to_name in columns.items():
+        try:
+            result[column_to_name] = list((dataframe.loc[dataframe[0] == column_to_find].to_dict()[1]).values())[0]
+        except Exception as ex:
+            result[column_to_name] = 'NA'
+    result = dataframe
 
 
-def process_data(symbol):
-    # create datetime objects
-    start = datetime.today() - timedelta(days=9125)
-    end = datetime.today()
-    # properly format the date to epoch time
-    start = format_date(start)
-    end = format_date(end)
-    # format the subdomain
-    sub = subdomain(symbol, start, end)
-    # customize the request header
-    hdrs = header(sub)
+def get_last_data_item(result, dataframe, columns):
+    data = dataframe.iloc[:, :2]
+    data.columns = ["Column", "Last"]
 
-    # concatenate the subdomain with the base URL
-    base_url = "https://finance.yahoo.com/quote/"
-    url = base_url + sub
+    for column_to_find, column_to_name in columns.items():
+        try:
+            val = data[data.Column.str.contains(column_to_find, case=False, regex=True)].iloc[0, 1]
+            float_val = to_float(val)
+            result[column_to_name] = float_val
+        except Exception as ex:
+            result[column_to_name] = "NA"
 
-    # scrape the dividend history table from Yahoo Finance
-    dividends = scrape_page(url, hdrs)
 
-    # clean the dividend history table
-    clean_div = clean_dividends(symbol, dividends[0])
-    return str(clean_div)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', threaded=True)
